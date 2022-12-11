4e6095c76a3df53b88411a83489e905393b77baf
nguyenanhtuanHDU <anhtuan02042002@gmail.com>
2022-12-11 10:18:18 +0700

    set dark light themes

--- assets/script/main.js
+++ assets/script/main.js
@@ -141,3 +141,15 @@ navRightBtn.addEventListener('click', function () {
 //     this.classList.add('fa-circle-chevron-left')
 //   }
 // })
+const body = document.querySelector('body')
+const toggle = document.querySelector('#toggle')
+const sunIcon = document.querySelector('.toggle .bxs-sun')
+const moonIcon = document.querySelector('.toggle .bx-moon')
+
+toggle.addEventListener('change', () => {
+  body.classList.toggle('dark')
+  sunIcon.className =
+    sunIcon.className == 'bx bxs-sun' ? 'bx bx-sun' : 'bx bxs-sun'
+  moonIcon.className =
+    moonIcon.className == 'bx bxs-moon' ? 'bx bx-moon' : 'bx bxs-moon'
+})
